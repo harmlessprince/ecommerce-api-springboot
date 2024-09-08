4236137be2fc074c5907524030a58b30cadb5c98
harmlessprince <realolamilekan@gmail.com>
2024-09-08 13:08:17 +0100

    user entity created

--- src/main/java/com/harmlessprince/ecommerceApi/EcommerceApiApplication.java
+++ src/main/java/com/harmlessprince/ecommerceApi/EcommerceApiApplication.java
@@ -2,8 +2,10 @@ package com.harmlessprince.ecommerceApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcommerceApiApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/harmlessprince/ecommerceApi/user/User.java
@@ -0,0 +1,35 @@
+package com.harmlessprince.ecommerceApi.user;
+
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
